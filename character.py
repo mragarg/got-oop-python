97a8b070bce7dff2ad6aded8e486417bef9d0cfd
Ashish Garg <mragarg@gmail.com>
2019-02-20 11:25:38 -0500

    Adds inventory attribute and tests it

--- character.py
+++ character.py
@@ -8,7 +8,8 @@
 class Character():
     
     # Constructor Method
-    def __init__(self, new_name, new_avatar):
+    def __init__(self, new_name, new_avatar, new_inventory):
         # 'self' is similar to 'this' in other languages
         self.name = new_name
         self.avatar = new_avatar
+        self.inventory = new_inventory
--- tests.py
+++ tests.py
@@ -3,8 +3,8 @@
 from character import Character
 
 # Characters can be instantiated with name and avatar
-arya = Character("Arya Stark", "arya.png")
-jon = Character("Jon Snow", "jon.png")
+arya = Character("Arya Stark", "arya.png", "sword")
+jon = Character("Jon Snow", "jon.png", "sword")
 
-print(arya.name, arya.avatar)
-print(jon.name, jon.avatar)
+print(arya.name, arya.avatar, arya.inventory)
+print(jon.name, jon.avatar, jon.inventory)
